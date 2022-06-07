b8ecd158c0e7977a9444826f6d95d4e8f0ec5417
felpk <mfrzbr@gmail.com>
2022-06-06 23:12:39 -0300

    commit 01

---
+++ app.js
@@ -0,0 +1,22 @@
+const MyApp = {
+    data (){
+        return {
+            name:"",
+            personagens: [],
+        }
+    },
+    mounted() {
+        fetch("https://rickandmortyapi.com/api/character")
+            .then((response) => response.json())
+            .then((data) => (this.personagens = data.results))
+    },
+    methods: {
+        search() {
+            fetch(`https://rickandmortyapi.com/api/character?name=${this.name}`)
+                .then((response) => response.json())
+                .then((data) => (this.personagens = data.results))
+        }
+    }
+}
+  
+  Vue.createApp(MyApp).mount("#app");
